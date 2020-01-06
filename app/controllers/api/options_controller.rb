4e7856d3549574e2a6c47b886298f817fd8df1c0
Cleveland Cooperative <cleveland.cooperative@gmail.com>
2020-01-06 16:06:44 -0500

    CRUD actions

---
+++ app/controllers/api/options_controller.rb
@@ -0,0 +1,69 @@
+class Api::OptionsController < ApplicationController
+before_action :authenticate_user, except: [:index, :show]
+
+  def index
+    @options = Option.all
+    render "index.json.jb"
+  end
+
+  def create
+    @option = Option.new(
+      title: params[:title],
+      description: params[:description],
+      industry: params[:industry],
+      email: params[:email],
+      phone: params[:phone],
+      user_id: current_user.id
+    )
+    if @option.save
+      render "show.json.jb"
+    else
+      render json: {errors: @option.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  def show
+    @option = Option.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @option = Option.find_by(id: params[:id])
+    @option.title = params[:title] || @option.title
+    @option.description = params[:description] || @option.description
+    if @option.save
+      render "show.json.jb"
+    else
+      render json: {errors: @option.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  def destroy
+    @option = Option.find_by(id: params[:id])
+    @option.destroy
+    render json: {message: "Option successfully destroyed!"}
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
